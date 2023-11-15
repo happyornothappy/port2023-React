c1e97b0789ca256f2dd709293d05771f04dbe85c
happyornothappy <101691696+happyornothappy@users.noreply.github.com>
2023-11-15 16:44:08 +0900

    라우팅 및 컴포넌트

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Main = () => {
+  return (
+    <div id="main" role="main">
+     
+    </div>
+  )
+}
+
+export default Main
